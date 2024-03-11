b64ab1ff1cda860244654b0db159817d3c550e51
HiroakiOkuda <hiroaki-okuda@gmo.jp>
2024-03-11 19:58:18 +0900

    fix: rewrite script

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,64 +1,40 @@
-# GitHub Secretsから必要なシークレットキー情報を取得
-KEY="$KEY"
-HOST="$HOST"
-USERNAME="$USERNAME"
-PORT="$PORT"
+#!/bin/bash
+# 必要な環境変数を設定
+KEY=$KEY
+HOST=$HOST
+USERNAME=$USERNAME
+PORT=$PORT
 
-# Conoha VPSへSSH接続するキーを設定
+# SSHキーのセットアップ
 mkdir -p ~/.ssh
-ssh-keyscan -p ${PORT} ${HOST} >> ~/.ssh/known_hosts
-echo "${KEY}" > ~/.ssh/deploy_key
+echo "$KEY" | tr -d '\r' > ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
+ssh-keyscan -p "$PORT" "$HOST" >> ~/.ssh/known_hosts
 
-# ロック問題の解決スクリプト
-clear_locks() {
-    echo "Checking for lock files..."
-    # Podman のロックファイルのパスを指定します。これはシステムによって異なる場合があります。
-    lock_path="/var/lib/containers/storage/overlay-images"
-
-    # ロックファイルが存在するか確認
-    if [ -d "$lock_path" ]; then
-        # ロックファイルを安全に削除
-        echo "Removing lock files..."
-        rm -f $lock_path/*.lock
-        echo "Lock files removed."
-    else
-        echo "No lock files found."
-    fi
-}
-
-# Podman サービスの再起動
-restart_podman() {
-    echo "Restarting Podman service..."
-    systemctl restart podman
-    echo "Podman service restarted."
-}
-
-# スクリプトの実行
-clear_locks
-restart_podman
-
-# ローカルでPodmanイメージをビルドしてリモートホストへ転送
+# リモートホストへのDockerイメージ転送
 podman build -t todoapp-server .
-podman save todoapp-server | gzip | ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" 'gunzip | podman load'
+podman save todoapp-server | gzip | ssh -i ~/.ssh/deploy_key -o BatchMode=yes ${USERNAME}@${HOST} -p ${PORT} 'gunzip | podman load'
 
-# SSHで接続し、Dockerコンテナの管理
-ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" << 'EOF'
+# コンテナの更新前にPodmanサービスを安全に再起動
+ssh -i ~/.ssh/deploy_key -o BatchMode=yes ${USERNAME}@${HOST} -p ${PORT} << EOF
+sudo systemctl restart podman.service
+EOF
 
-# Podmanコンテナの削除と実行
+# コンテナの更新と管理
+ssh -i ~/.ssh/deploy_key -o BatchMode=yes ${USERNAME}@${HOST} -p ${PORT} << 'EOF'
+
+# コンテナが存在する場合は削除
 podman rm -f todoapp-server || true
 
-if ! podman run -d --name=todoapp-server -p 3400:3400 todoapp-server; then
-    echo "Failed to run Podman container."
-    exit 1
-fi
+# 新しいコンテナを実行
+podman run -d --name=todoapp-server -p 3400:3400 todoapp-server
 
 EOF
 
-# エラーがあれば出力
+# デプロイメントの成功確認
 if [ $? -eq 0 ]; then
-    echo "Deployment succeeded"
-else 
-    echo "Deployment failed"
+    echo "Deployment succeeded."
+else
+    echo "Deployment failed."
     exit 1
 fi
