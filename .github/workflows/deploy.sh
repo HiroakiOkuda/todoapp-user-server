a2a8f4e3c503d7ca6e7ffc6c4728e1a09ce13461
HiroakiOkuda <hiroaki-okuda@gmo.jp>
2024-03-11 19:32:33 +0900

    fix: rewrite podman script

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,8 +1,8 @@
 # GitHub Secretsから必要なシークレットキー情報を取得
-KEY=$KEY
-HOST=$HOST
-USERNAME=$USERNAME
-PORT=$PORT
+KEY="$KEY"
+HOST="$HOST"
+USERNAME="$USERNAME"
+PORT="$PORT"
 
 # Conoha VPSへSSH接続するキーを設定
 mkdir -p ~/.ssh
@@ -10,19 +10,27 @@ ssh-keyscan -p ${PORT} ${HOST} >> ~/.ssh/known_hosts
 echo "${KEY}" > ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
 
-# ローカルでDockerイメージをビルドしてリモートホストへ転送
+# ローカルでPodmanイメージをビルドしてリモートホストへ転送
 podman build -t todoapp-server .
-podman save todoapp-server | gzip | ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} -p ${PORT} 'gunzip | podman load'
+podman save todoapp-server | gzip | ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" 'gunzip | podman load'
 
 # SSHで接続し、Dockerコンテナの管理
-ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} -p ${PORT} << 'EOF'
+ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" << 'EOF'
 
-# Dockerコンテナの削除と実行
-podman rm -f todoapp-server > /dev/null 2>&1 || echo "Container removal failed"
-podman run -d --name=todoapp-server -p 3400:3400 todoapp-server > /dev/null 2>&1 || echo "Failed to run Docker container"
+# Podmanコンテナの削除と実行
+podman rm -f todoapp-server || true
+
+if ! podman run -d --name=todoapp-server -p 3400:3400 todoapp-server; then
+    echo "Failed to run Podman container."
+    exit 1
+fi
+
+EOF
 
 # エラーがあれば出力
-if [ \$? -ne 0 ]; then
-    echo "Failed to run Docker container."
+if [ \$? -eq 0 ]; then
+    echo "Deployment succeeded"
+else 
+    echo "Deployment failed"
     exit 1
 fi
