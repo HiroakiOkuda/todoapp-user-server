3706ba3736c4b1ec7997e2ef1a8138cd04200822
HiroakiOkuda <hiroaki-okuda@gmo.jp>
2024-03-29 11:55:29 +0900

    fix: これでいける

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ const baseDbConfig = {
   type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER_NAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: ['dist/**/*.model{.ts,.js}'],
--- typeorm.config-migrations.js
+++ typeorm.config-migrations.js
@@ -7,14 +7,14 @@ require('dotenv').config({
 
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_PORT);
-console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_USER_NAME);
 console.log(process.env.DB_DATABASE);
 module.exports = [
   new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER_NAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: ['dist/**/*.model{.ts,.js}'],
