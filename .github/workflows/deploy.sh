2c8a7fc2542bebc91fbc52d15621f0c90aca2a14
HiroakiOkuda <hiroaki-okuda@gmo.jp>
2024-04-02 13:32:25 +0900

    fix: script

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -15,7 +15,7 @@ ssh-keyscan -p "$PORT" "$HOST" >> ~/.ssh/known_hosts
 podman build -t todoapp-server .
 podman save todoapp-server | gzip | ssh -i ~/.ssh/deploy_key -o BatchMode=yes ${USERNAME}@${HOST} -p ${PORT} 'gunzip | podman load'
 
-# リモートでのコンテナ操作
+# マイグレーションとコンテナの起動
 ssh -i ~/.ssh/deploy_key -p $PORT $USERNAME@$HOST << EOF
   cd todoapp-user-server
   git pull 
@@ -23,6 +23,11 @@ ssh -i ~/.ssh/deploy_key -p $PORT $USERNAME@$HOST << EOF
   env
   TODOAPP_ENV=production yarn migration:show
   TODOAPP_ENV=production yarn migration:run
+
+  # Docker コンテナの起動（ここを修正または追加）
+  podman stop todoapp-server || true  # 既に実行中のコンテナがあれば停止
+  podman rm todoapp-server || true  # 停止したコンテナを削除
+  podman run -d --name todoapp-server -p 3300:3300 todoapp-server  # 新しいコンテナを起動
 EOF
 
 # デプロイメントの成功確認
